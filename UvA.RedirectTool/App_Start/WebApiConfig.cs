55a30f46ecd8a23b8c16980dac2723903e082d28
Gerrit Oomens <g.oomens@uva.nl>
2018-02-01 00:24:00 +0100

    Initial commit

--- UvA.RedirectTool/App_Start/WebApiConfig.cs
+++ UvA.RedirectTool/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace UvA.RedirectTool
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
---
+++ UvA.RedirectTool/Controllers/LTIController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace UvA.RedirectTool.Controllers
+{
+    public class LTIController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Post(LTIData data)
+        {
+            return Redirect(data.custom_url.Replace("{id}", data.custom_id));
+        }
+    }
+
+    public class LTIData
+    {
+        public string custom_url { get; set; }
+        public string custom_id { get; set; }
+    }
+
+}
