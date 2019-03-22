75af5cc28d33b3b695fdd2353b39705d84d0ecf2
Gerrit Oomens <g.oomens@uva.nl>
2019-03-22 07:37:49 +0100

    Add hash

--- UvA.RedirectTool/Controllers/LTIController.cs
+++ UvA.RedirectTool/Controllers/LTIController.cs
@@ -1,18 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web.Http;
 
 namespace UvA.RedirectTool.Controllers
 {
     public class LTIController : ApiController
     {
+        static string Key = ConfigurationManager.AppSettings["Key"];
+        static HMACSHA1 Hash = new HMACSHA1(Encoding.ASCII.GetBytes(Key));
+
         [HttpPost]
         public IHttpActionResult Post(LTIData data)
         {
-            return Redirect(data.custom_url.Replace("{id}", data.custom_id));
+            return Redirect(data.custom_url.Replace("{id}", data.custom_id) + $"&hash={Convert.ToBase64String(Hash.ComputeHash(Encoding.ASCII.GetBytes(data.custom_id)))}");
         }
     }
 
